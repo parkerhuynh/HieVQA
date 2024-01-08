d45d32f515f4ad40c7851fa74278b08e72c54c3b
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-09 02:46:54 +1100

    vqa

--- metrics.py
+++ metrics.py
@@ -1,19 +1,6 @@
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 import json
 def calculate_vqa_accuracy(result_data):
-    """
-    Calculate the accuracy of VQA predictions based on large_question_type.
-
-    Parameters:
-    result_data (pd.result_dataFrame): result_dataFrame containing columns 'question_type', 'vqa', 
-                         'question_type_label', 'vqa_label', and 'large_question_type'.
-
-    Returns:
-    dict: A dictionary with 'large_question_type' as keys and accuracy as values.
-    """
-    
-    
-    
     
     accuracies = {}
     for lqt in result_data['answer_type'].unique():
@@ -27,8 +14,7 @@ def calculate_vqa_accuracy(result_data):
     formatted_accuracies = {
         f'{lqt.lower().replace("/","_")}_accuracy': accuracy for lqt, accuracy in accuracies.items()
     }
-    overall_correct_predictions = ((result_data['question_type'] == result_data['question_type_label']) & 
-                                   (result_data['vqa'] == result_data['vqa_label'])).sum()
+    overall_correct_predictions = ((result_data['prediction'] == result_data['target'])).sum()
     total_instances = len(result_data)
     vqa_accuracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
     formatted_accuracies.update({"accuracy_vqa":vqa_accuracy})
