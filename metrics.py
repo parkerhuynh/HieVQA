51fd2e04d7e7a6fc8162e93b8097b75b9963a47c
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-09 01:50:41 +1100

    vqa

--- metrics.py
+++ metrics.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 import json
-def calculate_vqa_accuracy(result_data, dataset):
+def calculate_vqa_accuracy(result_data):
     """
     Calculate the accuracy of VQA predictions based on large_question_type.
 
@@ -11,10 +11,7 @@ def calculate_vqa_accuracy(result_data, dataset):
     Returns:
     dict: A dictionary with 'large_question_type' as keys and accuracy as values.
     """
-    result_data["prediction class"] = result_data["prediction"].apply(lambda x: dataset.idx_to_ans(x))
-    result_data["target class"] = result_data["target"].apply(lambda x: dataset.idx_to_ans(x))
     
-    print(result_data)
     
     
     
@@ -38,7 +35,11 @@ def calculate_vqa_accuracy(result_data, dataset):
     formatted_accuracies.update({"val_accuracy_vqa":vqa_accuracy})
     return formatted_accuracies
 
-def calculate_accuracies(df):
+def calculate_accuracies(df, dataset):
+    df["prediction class"] = df["prediction"].apply(lambda x: dataset.idx_to_ans(x))
+    df["target class"] = df["target"].apply(lambda x: dataset.idx_to_ans(x))
+    
+    print(df)
     accuracy_type = accuracy_score(df['question_type_label'], df['question_type'])
     # # Calculate combined accuracy for VQA
     # correct_vqa_predictions = df[(df['question_type'] == df['question_type_label']) & 
