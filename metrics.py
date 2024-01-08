1497b70a83bf1631fd6a48c5712e1ec1fa2e5937
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-09 03:27:21 +1100

    vqa

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_sc
 import json
 def calculate_vqa_accuracy(result_data):
     
+    
     accuracies = {}
     for lqt in result_data['answer_type'].unique():
         df_lqt = result_data[result_data['answer_type'] == lqt]
@@ -21,10 +22,14 @@ def calculate_vqa_accuracy(result_data):
     return formatted_accuracies
 
 def calculate_accuracies(df, dataset):
+    with open("./dataset/super_answer_type_simpsons.json", 'r') as file:
+        super_type = json.load(file)
     df["prediction class"] = df["prediction"].apply(lambda x: dataset.ix_to_ans[str(x)])
     df["target class"] = df["target"].apply(lambda x: dataset.ix_to_ans[str(x)])
-    
     df["answer_type"] = df["id"].apply(lambda x: dataset.idx_to_ann[x]["answer_type"])
     
+    df["small_answer_type_target"] = df["target class"].apply(lambda x: super_type[x])
+    df["small_answer_type_prediction"] = df["prediction class"].apply(lambda x: super_type[x])
+    print(df)
     accuracy_vqa = calculate_vqa_accuracy(df)
     return accuracy_vqa
