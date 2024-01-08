f3402cda6bbfd42be18f651defe8f8a7d21a2aa8
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-09 02:14:35 +1100

    vqa

--- metrics.py
+++ metrics.py
@@ -36,7 +36,8 @@ def calculate_vqa_accuracy(result_data):
     return formatted_accuracies
 
 def calculate_accuracies(df, dataset):
-    print(dataset.idx_to_ans[str(x)])
+    print(dataset.idx_to_ans)
+    print(df)
     df["prediction class"] = df["prediction"].apply(lambda x: dataset.idx_to_ans[str(x)])
     df["target class"] = df["target"].apply(lambda x: dataset.idx_to_ans[str(x)])
     
