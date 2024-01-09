ee918ffccb05eceb1fe2387ecf5d97277d6ea03e
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-09 14:13:02 +1100

    vqa

--- running.py
+++ running.py
@@ -40,7 +40,7 @@ def main(args):
     print()
     # Initialize the distributed computing environment and other settings
     device, world_size = setup_environment(args)
-    if is_main_process() and args.save_model:
+    if is_main_process() and args.wandb:
         args.output_dir = output_path_create(args)
     if args.bs > 0:
         args.batch_size_train = int(float(args.bs)/world_size)
@@ -132,6 +132,7 @@ def main(args):
             val_loader = train_loader
         start_epoch = 0
         best_acc = 0
+        val_prediction_csv = None
         if is_main_process():
             print(model)
         for epoch in range(start_epoch, max_epoch):
@@ -140,7 +141,7 @@ def main(args):
                 train_loader.sampler.set_epoch(epoch)
             train_stats = trainer(model, train_loader, optimizer, loss_fn, epoch, device, lr_scheduler, args, wandb)
             
-            validation_stats, val_accuraciess, val_prediction_csv = validator(model, val_loader, device, loss_fn, args)
+            validation_stats, val_accuraciess, val_prediction_csv_i = validator(model, val_loader, device, loss_fn, args)
 
             if args.wandb:
                 wandb_train_log = {**{f'train_{k}': float(v) for k, v in train_stats.items()},
@@ -150,7 +151,8 @@ def main(args):
                 wandb.log(wandb_train_log)
 
 
-            if is_main_process() and args.save_model:
+            if is_main_process() and args.wandb:
+                val_prediction_csv = val_prediction_csv_i
                 if hasattr(model, 'module'):
                     model_without_ddp = model.module
                 #Save model
@@ -158,34 +160,33 @@ def main(args):
                 torch.save(model_without_ddp, last_model_path)
                 
                 if val_accuraciess['val_accuracy_vqa'] > best_acc:
-                    val_prediction_csv.to_csv("prediction.csv", index=False)
-                    directory = os.getcwd()
-                    file_path = os.path.join(directory, "prediction.csv")
-                    wandb.save(file_path, directory)
                     best_acc = val_accuraciess['val_accuracy_vqa']
                     best_model_path = os.path.join(args.output_dir, "best_model_state.pt")
                     torch.save(model_without_ddp, best_model_path)
                     
-                    
-                    y_true = val_prediction_csv['small_answer_type_target']
-                    y_pred = val_prediction_csv['small_answer_type_prediction']
-                    conf_matrix = confusion_matrix(y_true, y_pred, labels=y_true.unique())
-
-                    plt.figure(figsize=(10, 8))
-                    sns.heatmap(conf_matrix, annot=True, fmt='d', xticklabels=y_true.unique(), yticklabels=y_true.unique(), cmap='Blues')
-                    plt.title('Confusion Matrix')
-                    plt.xlabel('Predicted Label')
-                    plt.ylabel('True Label')
-                    buffer = BytesIO()
-                    plt.savefig(buffer, format='png')
-                    buffer.seek(0)
-                    image = Image.open(buffer)
-                    image_array = np.array(image)
-                    wandb.log({"Confusion Matrix": wandb.Image(image_array)})
-                    plt.close()
             print("\n")
         if is_main_process() and args.wandb:
+            val_prediction_csv.to_csv("prediction.csv", index=False)
             directory = os.getcwd()
+            file_path = os.path.join(directory, "prediction.csv")
+            wandb.save(file_path, directory)
+            
+            y_true = val_prediction_csv['small_answer_type_target']
+            y_pred = val_prediction_csv['small_answer_type_prediction']
+            conf_matrix = confusion_matrix(y_true, y_pred, labels=y_true.unique())
+
+            plt.figure(figsize=(10, 8))
+            sns.heatmap(conf_matrix, annot=True, fmt='d', xticklabels=y_true.unique(), yticklabels=y_true.unique(), cmap='Blues')
+            plt.title('Confusion Matrix')
+            plt.xlabel('Predicted Label')
+            plt.ylabel('True Label')
+            buffer = BytesIO()
+            plt.savefig(buffer, format='png')
+            buffer.seek(0)
+            image = Image.open(buffer)
+            image_array = np.array(image)
+            wandb.log({"Small Confusion Matrix": wandb.Image(image_array)})
+            plt.close()
             file_path = os.path.join(directory, "model.onnx")
             wandb.save(file_path, directory)
             
@@ -211,7 +212,6 @@ if __name__ == '__main__':
     parser.add_argument('--wandb', action='store_true')
     parser.add_argument('--wandb_dir', type=str, help="for fine-tuning")
     parser.add_argument('--checkpoint', type=str, default='')
-    parser.add_argument('--save_model', action='store_true')
 
     args = parser.parse_args()
 
