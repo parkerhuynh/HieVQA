0a059857648d3cdeb62c45b7d98d1a0a83546167
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-09 20:00:49 -0800

    vqa

--- metrics.py
+++ metrics.py
@@ -15,12 +15,32 @@ def calculate_vqa_accuracy(result_data):
     # Preparing to save accuracies with specified key names
     
     formatted_accuracies = {
-        f'val_{lqt.lower().replace("/","_")}_accuracy(vqa-wo-unans)': accuracy for lqt, accuracy in accuracies.items()
+        f'val_{lqt.lower().replace("/","_")}_accuracy(vqa-w-unans)': accuracy for lqt, accuracy in accuracies.items()
     }
     overall_correct_predictions = ((result_data['prediction'] == result_data['target'])).sum()
     total_instances = len(result_data)
     vqa_accuracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
+    formatted_accuracies.update({"val_accuracy_vqa(vqa-w-unans)":vqa_accuracy})
+
+    result_data = result_data[result_data["target class"] !="unanswerable"]
+    for lqt in result_data['answer_type'].unique():
+        df_lqt = result_data[result_data['answer_type'] == lqt]
+        correct_predictions = ((df_lqt['prediction'] == df_lqt['target'])).sum()
+        total_instances = len(df_lqt)
+        accuracies[lqt] = correct_predictions / total_instances if total_instances > 0 else 0
+    
+    # Preparing to save accuracies with specified key names
+    
+    formatted_accuracies_2 = {
+        f'val_{lqt.lower().replace("/","_")}_accuracy(vqa-wo-unans)': accuracy for lqt, accuracy in accuracies.items()
+    }
+    formatted_accuracies.update(formatted_accuracies_2)
+    overall_correct_predictions = ((result_data['prediction'] == result_data['target'])).sum()
+    total_instances = len(result_data)
+    vqa_accuracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
     formatted_accuracies.update({"val_accuracy_vqa(vqa-wo-unans)":vqa_accuracy})
+
+    
     return formatted_accuracies
 
 def calculate_accuracies(df, dataset):
@@ -32,6 +52,9 @@ def calculate_accuracies(df, dataset):
     
     df["small_answer_type_target"] = df["target class"].apply(lambda x: super_type[x])
     df["small_answer_type_prediction"] = df["prediction class"].apply(lambda x: super_type[x])
+    
+    df["birary answerable prediction"] = df["prediction class"].apply(lambda x: "unanswerable" if x == "unanswerable" else "answerable")
+    df["birary answerable tartget"] = df["target class"].apply(lambda x: "unanswerable" if x == "unanswerable" else "answerable")
 
     accuracy_vqa = calculate_vqa_accuracy(df)
     return accuracy_vqa, df
--- running.py
+++ running.py
@@ -156,8 +156,8 @@ def main(args):
                 last_model_path = os.path.join(args.output_dir, "model_latest_epoch.pt")
                 torch.save(model_without_ddp, last_model_path)
                 
-                if val_accuraciess['val_accuracy_vqa(vqa-wo-unans)'] > best_acc:
-                    best_acc = val_accuraciess['val_accuracy_vqa(vqa-wo-unans)']
+                if val_accuraciess['val_accuracy_vqa(vqa-w-unans)'] > best_acc:
+                    best_acc = val_accuraciess['val_accuracy_vqa(vqa-w-unans)']
                     wandb_log_val_accuracy_best = {**{f'best_{k}': v for k, v in val_accuraciess.items()}}
                     wandb.log(wandb_log_val_accuracy_best)
                     
@@ -213,6 +213,50 @@ def main(args):
             
             # Log the image to wandb
             wandb.log({"Confusion Matrix": wandb.Image(image_array)})
+            
+            # Close the plot
+            plt.close()
+
+            y_true = val_prediction_csv['birary answerable tartget']
+            y_pred = val_prediction_csv['birary answerable prediction']
+            conf_matrix = confusion_matrix(y_true, y_pred, labels=y_true.unique())
+            
+            # Normalize the confusion matrix
+            conf_matrix_normalized = conf_matrix.astype('float') / conf_matrix.sum(axis=1)[:, np.newaxis]
+            
+            # Set up the matplotlib figure for side-by-side plots
+            plt.figure(figsize=(30, 12))
+            
+            # Regular confusion matrix
+            plt.subplot(1, 2, 1)
+            sns.heatmap(conf_matrix, annot=True, fmt='d', 
+                        xticklabels=y_true.unique(), yticklabels=y_true.unique(), 
+                        cmap='Blues')
+            plt.title('Confusion Matrix')
+            plt.xlabel('Predicted Type')
+            plt.ylabel('True Type')
+            
+            # Normalized confusion matrix
+            plt.subplot(1, 2, 2)
+            sns.heatmap(conf_matrix_normalized, annot=True, fmt='.2f', 
+                        xticklabels=y_true.unique(), yticklabels=y_true.unique(), 
+                        cmap='Blues')
+            plt.title('Normalized Confusion Matrix')
+            plt.xlabel('Predicted Type')
+            plt.ylabel('True Type')
+            
+            # Main title
+            plt.suptitle(f'{args.task}-{args.version}-{args.version}-{args.dataset}-{args.task}-{args.created}')
+            
+            # Save the plot to a buffer
+            buffer = BytesIO()
+            plt.savefig(buffer, format='png')
+            buffer.seek(0)
+            image = Image.open(buffer)
+            image_array = np.array(image)
+            
+            # Log the image to wandb
+            wandb.log({"Unanswerable Confusion Matrix": wandb.Image(image_array)})
             
             # Close the plot
             plt.close()
