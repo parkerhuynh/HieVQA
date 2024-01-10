78a9c8175ba2b2ae1e8329157c4cddb17e5c07c1
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-09 19:12:34 -0800

    vqa

--- dataset/vqa_dataset_simpsons.py
+++ dataset/vqa_dataset_simpsons.py
@@ -114,7 +114,7 @@ class VQADataset(Dataset):
     
     def __len__(self):
         if self.args.debug:
-            return self.args.batch_size_train*2
+            return self.args.batch_size_train*4
         return len(self.annotations)
     
     
