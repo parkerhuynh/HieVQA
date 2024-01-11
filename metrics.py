ae6bfbd97bf20005fe1b668768357a5783dff91f
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-11 03:06:37 -0800

    vqa

--- metrics.py
+++ metrics.py
@@ -21,6 +21,20 @@ def calculate_vqa_accuracy(result_data):
     total_instances = len(result_data)
     vqa_accuracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
     formatted_accuracies.update({"val_accuracy_vqa(vqa-wo-unans)":vqa_accuracy})
+    
+    overall_correct_predictions = ((result_data['prediction'] == result_data['target'])).sum()
+    total_instances = len(result_data)
+    vqa_accuracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
+    
+    overall_correct_predictions = ((result_data['small_answer_type_target'] == result_data['small_answer_type_prediction'])).sum()
+    total_instances = len(result_data)
+    small_qt_accracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
+    formatted_accuracies.update({"small_qt_accracy(vqa-wo-unans)":small_qt_accracy})
+    
+    overall_correct_predictions = ((result_data['answer_type'] == result_data['answer_type_prediction'])).sum()
+    total_instances = len(result_data)
+    large_qt_accracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
+    formatted_accuracies.update({"large_qt_accracy(vqa-wo-unans)":large_qt_accracy})
     return formatted_accuracies
 
 def calculate_accuracies(df, dataset):
@@ -32,6 +46,9 @@ def calculate_accuracies(df, dataset):
     
     df["small_answer_type_target"] = df["target class"].apply(lambda x: super_type[x])
     df["small_answer_type_prediction"] = df["prediction class"].apply(lambda x: super_type[x])
+    
+    df["answer_type"] =  df["small_answer_type_target"].apply(lambda x: x if x in ["yes/no", "number"] else "other")
+    df["answer_type_prediction"] =  df["small_answer_type_prediction"].apply(lambda x: x if x in ["yes/no", "number"] else "other")
 
     accuracy_vqa = calculate_vqa_accuracy(df)
     return accuracy_vqa, df
