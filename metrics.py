a0f71488a6c628c89632500495e45b3851f3b31b
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-13 08:31:43 -0800

    Hie-VQA-W-Unans-9types

--- metrics.py
+++ metrics.py
@@ -17,16 +17,16 @@ def calculate_vqa_accuracy(result_data):
         f'val_{lqt.lower().replace("/","_")}_accuracy(vqa-w-unans)': accuracy for lqt, accuracy in accuracies.items()
     }
     ###########################################################
-    accuracies = {}
-    for lqt in result_data['answer_type'].unique():
-        df_lqt = result_data[result_data['answer_type'] == lqt]
-        correct_predictions = ((df_lqt['prediction class'] == df_lqt['target class'])).sum()
-        total_instances = len(df_lqt)
-        accuracies[lqt] = correct_predictions / total_instances if total_instances > 0 else 0
-    formatted_accuracies_1 = {
-        f'val_{lqt.lower().replace("/","_")}_accuracy(vqa-w-unans, origin)': accuracy for lqt, accuracy in accuracies.items()
-    }
-    formatted_accuracies.update(formatted_accuracies_1)
+    # accuracies = {}
+    # for lqt in result_data['answer_type'].unique():
+    #     df_lqt = result_data[result_data['answer_type'] == lqt]
+    #     correct_predictions = ((df_lqt['prediction class'] == df_lqt['target class'])).sum()
+    #     total_instances = len(df_lqt)
+    #     accuracies[lqt] = correct_predictions / total_instances if total_instances > 0 else 0
+    # formatted_accuracies_1 = {
+    #     f'val_{lqt.lower().replace("/","_")}_accuracy(vqa-w-unans, origin)': accuracy for lqt, accuracy in accuracies.items()
+    # }
+    # formatted_accuracies.update(formatted_accuracies_1)
     ###########################################################
     
     overall_correct_predictions = ((result_data['prediction class'] == result_data['target class'])).sum()
@@ -51,17 +51,17 @@ def calculate_vqa_accuracy(result_data):
     formatted_accuracies.update({"small_qt_accracy(vqa-w-unans)":small_qt_accracy})
     ###########################################################
     
-    overall_correct_predictions = ((result_data['answer_type'] == result_data['answer_type_prediction'])).sum()
-    total_instances = len(result_data)
-    small_qt_accracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
-    formatted_accuracies.update({"large_qt_accracy(vqa-w-unans, originn)":small_qt_accracy})
+    # overall_correct_predictions = ((result_data['answer_type'] == result_data['answer_type_prediction'])).sum()
+    # total_instances = len(result_data)
+    # small_qt_accracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
+    # formatted_accuracies.update({"large_qt_accracy(vqa-w-unans, originn)":small_qt_accracy})
 
     overall_correct_predictions = ((result_data['processed_answer_type'] == result_data['processed_answer_type_prediction'])).sum()
     total_instances = len(result_data)
     small_qt_accracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
     formatted_accuracies.update({"large_qt_accracy(vqa-w-unans)":small_qt_accracy})
     
-    result_data = result_data[result_data["processed_answer_type"] !="unanswerable"]
+    result_data = result_data[result_data["target class"] !="unanswerable"]
     accuracies = {}
     for lqt in result_data['answer_type'].unique():
         df_lqt = result_data[result_data['answer_type'] == lqt]
@@ -92,10 +92,10 @@ def calculate_vqa_accuracy(result_data):
     formatted_accuracies.update({"small_qt_accracy(vqa-wo-unans)":small_qt_accracy})
     ###########################################################
     
-    overall_correct_predictions = ((result_data['answer_type'] == result_data['answer_type_prediction'])).sum()
-    total_instances = len(result_data)
-    small_qt_accracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
-    formatted_accuracies.update({"large_qt_accracy(vqa-wo-unans, originn)":small_qt_accracy})
+    # overall_correct_predictions = ((result_data['answer_type'] == result_data['answer_type_prediction'])).sum()
+    # total_instances = len(result_data)
+    # small_qt_accracy = overall_correct_predictions / total_instances if total_instances > 0 else 0
+    # formatted_accuracies.update({"large_qt_accracy(vqa-wo-unans, originn)":small_qt_accracy})
 
     overall_correct_predictions = ((result_data['processed_answer_type'] == result_data['processed_answer_type_prediction'])).sum()
     total_instances = len(result_data)
@@ -122,7 +122,7 @@ def calculate_accuracies(df, dataset):
     df["answer_type"] =  df["small_answer_type_target"].apply(lambda x: x if x in ["yes/no", "number"] else "other")
     df["answer_type_prediction"] =  df["small_answer_type_prediction"].apply(lambda x: x if x in ["yes/no", "number"] else "other")
     
-    df["processed_answer_type"] =  df["small_answer_type_target"].apply(lambda x: x if x in ["yes/no", "number", "unanswerable"] else "other")
+    df["processed_answer_type"] =  df["id"].apply(lambda x: x if x in ["yes/no", "number", "unanswerable"] else "other")
     df["processed_answer_type_prediction"] =  df["small_answer_type_prediction"].apply(lambda x: x if x in ["yes/no", "number", "unanswerable"] else "other")
     
 
