c3e688a4279a8b76527aa6564970eebc97a2a3bd
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-28 21:05:58 +1100

    Hie-VQA-Wo-Unans-9types

--- loss.py
+++ loss.py
@@ -33,6 +33,6 @@ class HierarchicalLoss(nn.Module):
             vqa_loss_i = vqa_loss_i.mean()
             vqa_losses[question_type_str] = vqa_loss_i
         # total_vqa_loss = sum(vqa_losses.values())
-        total_loss = qt_loss + total_vqa_loss
+        # total_loss = qt_loss + total_vqa_loss
         # return qt_loss, total_vqa_loss, total_loss
         return qt_loss, vqa_losses
