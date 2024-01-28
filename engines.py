0a3d2b6585c17383c7f34805561c4d7cf0bd72f0
parkerhuynh <dunghuynh110496@gmail.com>
2024-01-28 21:45:05 +1100

    Hie-VQA-Wo-Unans-9types

--- engines.py
+++ engines.py
@@ -34,6 +34,7 @@ def trainer(model, data_loader, optimizer, loss_function, epoch, device, args, w
         optimizer.zero_grad()
         qt_loss.backward()
         for vqa_based_qt in vqa_losses:
+            print(vqa_losses[vqa_based_qt])
             vqa_losses[vqa_based_qt].backward(retain_graph=True)
         
         
